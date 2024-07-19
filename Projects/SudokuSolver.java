b557e94ec0ab754fd5fb213f3a37b95df5d29cfe
LostZenith95 <denzell.mackay@hotmail.com>
2024-07-19 14:58:29 -0400

    Created method to find number anywhere in the game board

--- Projects/SudokuSolver.java
+++ Projects/SudokuSolver.java
@@ -1,6 +1,10 @@
 package JavaLearning.Projects;
 
 public class SudokuSolver {
+    /**
+     * This constant represents the board size.
+     * GRID_SIZE x GRID_SIZE;
+     */
     private static final int GRID_SIZE = 9;
 
     /**
@@ -10,9 +14,9 @@ public class SudokuSolver {
      * @param board: A 2D array of numbers, representing a 9x9 Sudoku board.
      * @param number: Number given to be checked against row number.
      * @param row: The rows of the board
-     * @return
+     * @return Boolean
      */
-    private boolean isNumberInRow(int[][] board, int number, int row) {
+    private static boolean isNumberInRow(int[][] board, int number, int row) {
         for(int i = 0; i < GRID_SIZE; i++) {
             if (board[row][i] == number) {
                 return true;
@@ -28,9 +32,9 @@ public class SudokuSolver {
      * @param board: A 2D array of numbers, representing a 9x9 Sudoku board.
      * @param number: Number given to be checked against column number.
      * @param column: The column of the board
-     * @return
+     * @return Boolean
      */
-    private boolean isNumberInColumn(int[][] board, int number, int column) {
+    private static boolean isNumberInColumn(int[][] board, int number, int column) {
         for(int i = 0; i < GRID_SIZE; i++) {
             if (board[i][column] == number) {
                 return true;
@@ -39,23 +43,44 @@ public class SudokuSolver {
         return false;
     }
 
-    private boolean isNumberInBox(int[][] board, int number, int row, int column) {
-        for()
+    /**
+     * This method is searching 'board' to find 'number' in either 'row' position or 'column' position.
+     * When this method finds the 'number' it returns true, otherwise false.
+     *
+     * @param board: A 2D array of numbers, representing a 9x9 Sudoku board.
+     * @param number: Number given to be checked against column number.
+     * @param row: The rows of the board
+     * @param column: The column of the board
+     * @return Boolean
+     */
+    private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
+        int localBoxRow = row - row % 3;
+        int localBoxColumn = column - column % 3;
+
+        for (int i = localBoxRow; i < localBoxRow + 3; i++) {
+            for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
+                if(board[row][column] == number) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
 
     public static void main(String[] args) {
         int [][] board = {
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,0,0}
+                {4, 0, 0, 2, 0, 0, 3, 1, 8},
+                {0, 0, 0, 9, 0, 1, 2, 0, 0},
+                {6, 2, 1, 0, 3, 0, 5, 4, 9},
+                {0, 0, 0, 8, 6, 2, 0, 0, 0},
+                {2, 6, 9, 1, 0, 5, 0, 0, 0},
+                {0, 0, 8, 0, 0, 4, 0, 0, 0},
+                {0, 4, 0, 0, 0, 9, 0, 0, 5},
+                {0, 0, 2, 0, 0, 0, 4, 0, 6},
+                {9, 5, 0, 0, 8, 7, 1, 0, 0}
         };
+
+        System.out.println(isNumberInBox(board, 8, 5, 2));
     }
 }
